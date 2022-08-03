abfb7adf6ef6a355c48add9aa4d6e8cf8f55e0e9
harshthakkar09 <thakkarharsh1999@gmail.com>
2022-08-03 22:06:23 +0530

    added login functionality

--- controller/authController.go
+++ controller/authController.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"log"
 	"regexp"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/harshthakkar09/blog-website/database"
 	"github.com/harshthakkar09/blog-website/models"
+	"github.com/harshthakkar09/blog-website/util"
 )
 
 func validateEmail(email string) bool {
@@ -64,5 +67,42 @@ func Register(c *fiber.Ctx) error {
 	return c.JSON(fiber.Map{
 		"user":    user,
 		"message": "Account created successfully",
+	})
+}
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		fmt.Println("Unable to parse body")
+	}
+	var user models.User
+	database.DB.Where("email=?", data["email"]).First(&user)
+	if user.Id == 0 {
+		c.Status(404)
+		return c.JSON(fiber.Map{
+			"message": "Email Address doesn't exists, kindly create an account",
+		})
+	}
+	if err := user.ComparePassword(data["password"]); err != nil {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "incorrect password",
+		})
+	}
+	token, err := util.GenerateJwt(strconv.Itoa(int(user.Id)))
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError)
+		return nil
+	}
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 1),
+		HTTPOnly: true,
+	}
+	c.Cookie(&cookie)
+	return c.JSON(fiber.Map{
+		"message": "you successfully login",
+		"user":    user,
 	})
 }
--- routes/routes.go
+++ routes/routes.go
@@ -7,4 +7,6 @@ import (
 
 func Setup(app *fiber.App) {
 	app.Post("api/register", controller.Register)
+	app.Post("api/login", controller.Login)
+	// app.Use(middleware.IsAuthanticate)
 }
